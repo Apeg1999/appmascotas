53e9ddbf7d89f27a16c5800a12738f0834f02766
Apeg1999 <alison.elgueta@inacapmail.cl>
2025-08-30 00:32:58 -0400

    Corregir configuración de Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/catfact-api': {
-        target: 'https://catfact.ninja',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/catfact-api/, '/fact'),
-      },
-      '/deepl-proxy': {
-        target: 'https://api-free.deepl.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/deepl-proxy/, '/v2/translate'),
-        headers: {
-          'Authorization': 'DeepL-Auth-Key f4b8327f-a345-4712-96ba-27038e19c4ae:fx'
-        },
-      },
-    }
-  }
+  // Esta línea es crucial para que Vite sepa dónde está tu proyecto en GitHub Pages
+  base: "/appmascotas/",
+  plugins: [react()],
 });
